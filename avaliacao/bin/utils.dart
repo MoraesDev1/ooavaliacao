d03b1a39f2ed65592e1ac6ffebc4a470c8388c33
Gustavo Morais <gu.herber@gmail.com>
2023-11-08 21:31:04 -0300

    datetime format

--- avaliacao/bin/utils.dart
+++ avaliacao/bin/utils.dart
@@ -1,3 +1,13 @@
+import 'package:intl/intl.dart';
+
 class Utils {
   // implementar formatação de dataNascimento
+  todayDate() {
+    var now = new DateTime.now();
+    var formatter = new DateFormat('dd-MM-yyyy');
+    String formattedTime = DateFormat('kk:mm:a').format(now);
+    String formattedDate = formatter.format(now);
+    print(formattedTime);
+    print(formattedDate);
+  }
 }
